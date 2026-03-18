b2c1d9cdc1d9bc9506c820c629480a2b4854bea7
Eliaselnocapo <misarb46@gmail.com>
2026-03-18 00:09:20 -0600

    AAAA si quedo

--- Arraysypunteros/XD.cpp
+++ Arraysypunteros/XD.cpp
@@ -16,7 +16,7 @@ int Verificaciones(){
 
 void Recibir(int* arr, int tam){
     for(int i=0; i <tam;i++){
-        cout << "Dame el elemento " << i << endl; 
+        cout << "Dame el elemento " << i+1 << endl; 
         cin >> arr[i];
     }
 
@@ -30,22 +30,49 @@ cout<< endl;
 }
 
 
-int Posicion(int*arr, int tam,int &inicio,int &final){
+int Posicion(int*arr,int inicio,int final){
+int pivote = arr[final];
+int i = inicio - 1;
+for (int j = inicio; j< final; j++){
+    if(arr[j]<= pivote){
+        i++;
+        swap(arr[i],arr[j]);
+    }
+}
+swap(arr[i+1],arr[final]);
+return(i+1);
+}
+
+void Quicksort(int*arr, int inicio,int final){
+    if(inicio< final){
+        int pi = Posicion(arr,inicio,final);
+        Quicksort(arr,inicio,pi-1);
+        Quicksort(arr,pi+1,final);
+    }
+}
+
+void P_Final(int* arr, int tam){
+
+    cout << "El arreglo ordenado es "<< endl;
+
+            for(int i=0; i <tam;i++){
+                cout << arr[i]<<" ";
+            }
+    cout<< endl;
 
 }
 
-
-
 int main(){
-int Tamano;
-int inicio=0;
-Tamano=Verificaciones();
-int final=Tamano-1;
+    int Tamano;
+    int inicio=0;
+        Tamano=Verificaciones();
+    int final=Tamano-1;
 
 
-int* Lista=new int[Tamano];
-Recibir (Lista,Tamano);
-
-delete[] Lista;
+    int* Lista=new int[Tamano];
+        Recibir (Lista,Tamano);
+        Quicksort(Lista,inicio,final);
+        P_Final(Lista,Tamano);
+    delete[] Lista;
 return 0;
 }
